d5001d038a9b679998731f57e0cd58d33958d7ed
Brian Jenkins <bonkydog@bonkydog.com>
2014-06-10 12:29:34 -0700

    Float Datomic version, bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.goodguide/datomizer "0.1.0"
+(defproject com.goodguide/datomizer "0.2.0"
   :description "Simple Datomic adapter and marshalling for JRuby"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-pro "0.9.4707"]
+                 [com.datomic/datomic-pro "[0.9,1.0)"]
                  [org.clojure/test.check "0.5.7"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
